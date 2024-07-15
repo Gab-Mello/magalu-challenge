f9c879dcbfdc655c51f31497e6d3f42d0d0af009
Gabriel Mello <gabrielpontemello1@gmail.com>
2024-07-14 21:08:13 -0300

    find schedule by id

--- src/main/java/com/gabriel/magalu/controllers/CommunicationController.java
+++ src/main/java/com/gabriel/magalu/controllers/CommunicationController.java
@@ -1,10 +1,15 @@
 package com.gabriel.magalu.controllers;
 
 import com.gabriel.magalu.dtos.ScheduleDto;
+import com.gabriel.magalu.entities.Communication;
 import com.gabriel.magalu.services.CommunicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api/notifications")
@@ -18,5 +23,17 @@ public class CommunicationController {
         communicationService.scheduleCommunication(dto);
 
         return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Communication> getNotification(@PathVariable Long id){
+
+        Optional<Communication> communication = communicationService.findById(id);
+
+        if (communication.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(communication.get());
     }
 }
--- src/main/java/com/gabriel/magalu/services/CommunicationService.java
+++ src/main/java/com/gabriel/magalu/services/CommunicationService.java
@@ -1,9 +1,12 @@
 package com.gabriel.magalu.services;
 
 import com.gabriel.magalu.dtos.ScheduleDto;
+import com.gabriel.magalu.entities.Communication;
 import com.gabriel.magalu.repositories.CommunicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CommunicationService {
@@ -13,5 +16,9 @@ public class CommunicationService {
 
     public void scheduleCommunication(ScheduleDto dto){
         communicationRepository.save(dto.toCommunication());
+    }
+
+    public Optional<Communication> findById(Long id){
+        return communicationRepository.findById(id);
     }
 }
